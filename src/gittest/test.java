1a3946d92b1e7f72826ad1a6bb2d500435431f90
guoxingze <guoxingze@gmail.com>
2016-02-14 17:05:13 -0600

    modified test

--- src/gittest/test.java
+++ src/gittest/test.java
@@ -1,5 +1,35 @@
 package gittest;
+import junit.framework.*;
 
-public class test {
+public class test extends TestCase{
+	int testGetInt(int a){
+		return a;
+	}
+
+public int convertInt2String(String input) throws Exception{
+	int result = 0;
+	if(input!=null && !input.equals("")){
+		result = Integer.parseInt(input);
+	}else{
+		throw new Exception("Invalid input");
+	}
+	return result;
+}	
+	
+public static void main(String [ ] args){
+	test test1 = new test();
+	String s = "";
+	int result = 0;
+	try {
+		result = test1.convertInt2String(s);
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	System.out.println(result);
+}
+
 
 }
+
+
